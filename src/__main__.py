46c404f54da95d6c3a5579fba22c30ef07e1033b
SDarkMagic <dragonbuilderx@gmail.com>
2021-09-06 22:56:33 -0400

    Initialize Repo

---
+++ src/__main__.py
@@ -0,0 +1,11 @@
+import twitchio
+import os
+from twitchio.ext import commands
+
+bot = commands.Bot(
+    irc_token=os.environ['TMI_TOKEN'],
+    client_id=os.environ['CLIENT_ID'],
+    nick=os.environ['BOT_NICK'],
+    prefix=os.environ['BOT_PREFIX'],
+    initial_channels=['ZoteTheMightyBot']
+)
