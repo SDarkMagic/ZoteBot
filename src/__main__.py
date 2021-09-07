e7bb7778bb15c78902d1a3175b762ebdb8864145
SDarkMagic <60792071+SDarkMagic@users.noreply.github.com>
2021-09-07 12:27:11 +0000

    Started basic bot framework

--- src/__main__.py
+++ src/__main__.py
@@ -3,9 +3,17 @@ import os
 from twitchio.ext import commands
 
 bot = commands.Bot(
-    irc_token=os.environ['TMI_TOKEN'],
+    token=os.environ['TMI_TOKEN'],
     client_id=os.environ['CLIENT_ID'],
     nick=os.environ['BOT_NICK'],
     prefix=os.environ['BOT_PREFIX'],
     initial_channels=['ZoteTheMightyBot']
 )
+
+@bot.event()
+async def event_ready(ctx):
+  print('Zote has entered the arena and is preparing for combat...')
+  await __ws.send_privmsg('ZoteTheMightyBot', 'Booptis')
+
+if __name__ == '__main__':
+  bot.run()
